7b269bf6164cdc305adb8ce6259b2d8fcafe88e7
alexandre gaillot <gaillotalexandre@yahoo.fr>
2022-04-12 13:25:57 +0200

    save categorie & README

--- README.md
+++ README.md
@@ -5,14 +5,50 @@ Ce projet est réalisé avec la promotion **altrh dwwm19** de l'année 2022.
 Vous pouvez retrouver le projet sur <https://github.com/alexgaill/dwwm19-symfony>.
 
 ## Pour lancer le projet:
+
 > Chargez les dépendances avec composer
+>
+> ``` 
+> composer install 
+> ```
+>
+> Modifiez la ligne du fichier *.env* pour la connexion à la BDD
+>
+> ``` 
+> DATABASE_URL="mysql://root:@127.0.0.1:3306/superblog" # Pour Wamp et Xampp
+>
+> DATABASE_URL="mysql://root:root@127.0.0.1:8889/superblog" # Pour Mamp 
+> ```
+>
+> Créez la BDD
+>
+> ``` 
+> symfony console doctrine:database:create 
+> ```
+>
+> Ajoutez les données en BDD
+>
+> ```
+> symfony console doctrine:migrations:migrate
+> 
+> symfony console doctrine:fixtures:load 
+> ```
+>
+> Lancez le serveur
+>
+> ``` 
+> symfony server:start 
+> ```
 
-```
-composer install
-```
+## Liens utiles 
 
-- 
+[Documentation Symfony](https://symfony.com/doc/current/index.html)
 
+[Documentation Twig](https://twig.symfony.com/)
+
+[Documentation doctrine](https://www.doctrine-project.org/projects/orm.html)
+
+[Fakerphp](https://fakerphp.github.io/)
 
 ## Une question sur le projet? 
 [Contactez-moi](mailto:contact@steptosuccess.com)
@@ -20,8 +56,8 @@ composer install
 ## Pour aller plus loin vous pouvez:
 
 > ### Rejoindre la communauté sur [discord](https://discord.gg/rb4bVeZX)
-> Vous serez avertis des dernières news de cours, vous agrandirez la communauté de debug.
+> Vous serez avertis des dernières news de cours, vous agrandirez la communauté d'entraide d'étudiants et devs juniors.
 >
-> ### Participer aux lives [twitch]()
+> ### Participer aux lives [twitch](https://www.twitch.tv/alex_gaill)
 > Live de création et de mise à jour de cours, découvertes de langages et framework, talkshow sur différents sujets (S'améliorer en temps que dev, le freelancing, la recherche d'emploi/stage/alternance)
 
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Category;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,5 +38,46 @@ class CategoryController extends AbstractController
             // Sinon on redirige l'utilisateur vers la page contenant toutes les catégories
             return $this->redirectToRoute('app_category');
         }
+    }
+    
+    #[Route('/category/save', name: 'save_category', methods:["GET", "POST"])]
+    public function save (Request $request, ManagerRegistry $manager): Response
+    {
+        // On créé une nouvelle catégorie
+        $category = new Category;
+        // On créé le formulaire auquel on associe la catégorie qui récupèrera les informations
+        $form = $this->createFormBuilder($category)
+        // On ajoute l'input pour le name de la catégorie
+        // Attention n'oubliez pas le: use Symfony\Component\Form\Extension\Core\Type\TextType;
+                ->add('name', TextType::class, [
+                    'label' => 'Nom de la catégorie',
+                    'required'=> true
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => "Ajouter",
+                    'attr' => [
+                        'class' => "btn btn-primary"
+                    ]
+                ])
+                // Génère l'objet formulaire
+                ->getForm();
+        // Associe les données provenant de $_POST à notre formulaire 
+        // et plus particulièrement à la catégorie associée
+        $form->handleRequest($request);
+        // $form->isSubmitted() vérifie que le formulaire a bien été soumis par le button
+        // $form->isValid() vérifie que les données reçues correspondent à toutes les contraintes indiquées 
+        // pour chaque propriété
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $em = $manager->getManager();
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->renderForm('category/save.html.twig',[
+            'formCategory' => $form,
+            'category' => $category
+        ]);
     }
 }
